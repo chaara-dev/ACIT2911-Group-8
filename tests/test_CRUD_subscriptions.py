1fee7a0d77ad2f8579f722877e1f64f7c385f001
Leon Xue <lxue18@my.bcit.ca>
2026-05-07 10:49:32 -0700

    POST tests

--- tests/test_CRUD_subscriptions.py
+++ tests/test_CRUD_subscriptions.py
@@ -7,3 +7,21 @@ from api import create_app
 def client():
     app = create_app()
     return app.test_client()
+
+# POST tests
+def test_create_subscription(client):
+    response = client.post("/api/subscriptions", json={
+        "user_id": 1,
+        "name": "Netflix",
+        "cost": 12.99,
+        "billing_type": "monthly"
+    })
+    assert response.status_code == 201
+ 
+ 
+def test_create_unfinished_subscription(client):
+    response = client.post("/api/subscriptions", json={
+        "name": "Netflix"
+    })
+    assert response.status_code == 400
+
