c4e4805856817c6b1804640ed212c9f1b8dd8d84
Rajveer <singhrajveer4021@gmail.com>
2026-05-10 15:43:40 -0700

    create view routes to load/render html pages

--- api/views/routes.py
+++ api/views/routes.py
@@ -1,1 +1,23 @@
+from flask import render_template
+
 from . import views_bp
+
+from flask import render_template
+
+from . import views_bp
+
+@views_bp.route("/")
+def index():
+    return render_template("index.html")
+
+@views_bp.route("/subscription-create")
+def subscription_create():
+    return render_template("subscription-create.html")
+
+@views_bp.route("/subscription-view")
+def subscription_view():
+    return render_template("subscription-view.html")
+
+@views_bp.route("/subscription-edit")
+def subscription_edit():
+    return render_template("subscription-edit.html")
