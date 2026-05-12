85a51151401c2778fc0e7568242a0ea080f3e565
Rajveer <singhrajveer4021@gmail.com>
2026-05-11 20:41:52 -0700

    add function that registers a new user account and redirects to login page

---
+++ static/js/signup.js
@@ -0,0 +1,49 @@
+const displayError = (errorMessage) => {
+  const signupButton = document.querySelector("button");
+
+  if (!document.querySelector(".signup-error")) {
+    const error = document.createElement("p");
+    error.textContent = errorMessage;
+    error.classList.add("signup-error"); // edit this class in signup.css
+    signupButton.after(error);
+  }
+};
+
+const createNewAccount = async () => {
+  const signupButton = document.querySelector("button");
+
+  signupButton.addEventListener("click", async () => {
+    const email = document.querySelector(`[type="email"]`).value;
+    const password = document.querySelector(`[type="password"]`).value;
+
+    if (!email || !password) {
+      return displayError("Missing email or password");
+    }
+
+    const newAccount = { email: email, password: password };
+
+    try {
+      const res = await fetch("/api/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newAccount),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data["error"]);
+      }
+      window.location.href = "/login";
+    } catch (error) {
+      console.log(error);
+      return displayError(error);
+    }
+  });
+};
+
+const main = async () => {
+  createNewAccount();
+};
+
+main();
