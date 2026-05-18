004cf952fa09e707cdb248c2d0c04f69d54ffe1e
Rajveer Singh <singhrajveer4021@gmail.com>
2026-05-17 17:59:18 -0700

    add function to view/edit sub and placeholder css

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -95,6 +95,22 @@ const displaySubscriptions = (subs) => {
   });
 };
 
+const resetDialog = () => {
+  document.getElementById("subPrice").hidden = false;
+  document.getElementById("subPriceEdit").hidden = true;
+  document.getElementById("subPeriod").hidden = false;
+  document.getElementById("subPeriodEdit").hidden = true;
+  document.getElementById("subRenewalDate").hidden = false;
+  document.getElementById("subRenewalDateEdit").hidden = true;
+  document.getElementById("subscribedOn").hidden = false;
+  document.getElementById("subscribedOnDateEdit").hidden = true;
+  document.getElementById("editSubButtonDiv").hidden = false;
+  document.getElementById("saveEditButtonDiv").hidden = true;
+  document.getElementById("cancelEditButtonDiv").hidden = true;
+  document.querySelector(".sub-total-paid").hidden = false;
+  document.querySelector(".sub-payments").hidden = false;
+};
+
 const displaySubscription = async () => {
   const viewSubPage = document.getElementById("viewSubscription");
   const subCard = document.querySelectorAll(".card");
@@ -164,10 +180,78 @@ const displaySubscription = async () => {
       // });
 
       viewSubPage.showModal();
+
+      const displayEditSubscription = () => {
+        const editButton = document.getElementById("editSubButton");
+        editButton.addEventListener(
+          "click",
+          () => {
+            document.querySelector(".sub-total-paid").hidden = true;
+            document.querySelector(".sub-payments").hidden = true;
+
+            document.getElementById("subPrice").hidden = true;
+            document.getElementById("subPriceEdit").hidden = false;
+            const priceValue = document.getElementById("subPriceEditInput");
+            priceValue.value = sub.cost;
+
+            document.getElementById("subPeriod").hidden = true;
+            document.getElementById("subPeriodEdit").hidden = false;
+            const periodValue = document.getElementById("subPeriodEditInput");
+            periodValue.value = sub.billing_type;
+
+            document.getElementById("subRenewalDate").hidden = true;
+            document.getElementById("subRenewalDateEdit").hidden = false;
+            const renewalDateValue = document.getElementById(
+              "subRenewalDateInput",
+            );
+            renewalDateValue.value = sub.renewal_date.slice(0, 10);
+
+            document.getElementById("subscribedOn").hidden = true;
+            document.getElementById("subscribedOnDateEdit").hidden = false;
+
+            document.getElementById("editSubButtonDiv").hidden = true;
+            document.getElementById("saveEditButtonDiv").hidden = false;
+            const saveButton = document.getElementById("saveEditButton");
+            document.getElementById("cancelEditButtonDiv").hidden = false;
+            const cancelButton = document.getElementById("cancelEditButton");
+
+            saveButton.addEventListener(
+              "click",
+              async () => {
+                const edits = {
+                  name: sub.name,
+                  cost: priceValue.value,
+                  billing_type: periodValue.value,
+                  renewal_date: renewalDateValue.value,
+                };
+                const res = await fetch(`/api/subscriptions/${subId}`, {
+                  method: "PUT",
+                  headers: { "Content-Type": "application/json" },
+                  body: JSON.stringify(edits),
+                });
+                window.location.href = `/`;
+              },
+              { once: true },
+            );
+
+            cancelButton.addEventListener(
+              "click",
+              () => {
+                resetDialog();
+                displayEditSubscription();
+              },
+              { once: true },
+            );
+          },
+          { once: true },
+        );
+      };
+      displayEditSubscription();
     });
   });
 
   closeSubButton.addEventListener("click", () => {
+    resetDialog();
     viewSubPage.close();
   });
 };
