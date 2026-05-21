bc30aaf4ed4c66fedf1017ec11706b3beea31f60
Leon Xue <lxue18@my.bcit.ca>
2026-05-20 19:58:49 -0700

    init client

--- tests/functional/test_auth.py
+++ tests/functional/test_auth.py
@@ -2,3 +2,8 @@ import pytest
 from src.api import create_app
 from src.database.models import User
 
+@pytest.fixture()
+def user_client():
+    flask_app = create_app()
+    with flask_app.test_client() as testing_client:
+        yield testing_client
