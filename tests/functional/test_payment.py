82e13f2634f9583ea621482f6652eb6b37229691
Leon Xue <lxue18@my.bcit.ca>
2026-05-20 22:30:47 -0700

    init client

--- tests/functional/test_payment.py
+++ tests/functional/test_payment.py
@@ -2,3 +2,9 @@ import pytest
 from src.api import create_app
 from src.database.models import User, Subscription, Payment
 from datetime import date
+
+@pytest.fixture()
+def payment_client():
+    flask_app = create_app()
+    with flask_app.test_client() as testing_client:
+        yield testing_client
