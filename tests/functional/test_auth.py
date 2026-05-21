4c8ecc87bf7a550a1199605fc5002ebc246fd7c4
Leon Xue <lxue18@my.bcit.ca>
2026-05-20 20:39:11 -0700

    test login

--- tests/functional/test_auth.py
+++ tests/functional/test_auth.py
@@ -8,6 +8,7 @@ def user_client():
     with flask_app.test_client() as testing_client:
         yield testing_client
 
+#register
 def test_register(user_client):
     response = user_client.post(
         "/api/register",
@@ -31,4 +32,18 @@ def test_register_without_password(user_client):
     data = response.get_json()
     assert data["error"] == "email and password are required"
 
+#login
+def test_login(user_client):
+    user_client.post(
+        "/api/register",
+        json={"email": "new_user@test.com", "password": "password123"}
+    )
     
+    response = user_client.post(
+        "/api/login",
+        json={"email": "new_user@test.com", "password": "password123"}
+    )
+    assert response.status_code == 200
+    
+    test_user = User.get(User.email == "new_user@test.com")
+    test_user.delete_instance()
